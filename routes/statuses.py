e9528113c6fa084aa1d7746d8849a1b24403fda1
Vianpyro <vianney@veremme.org>
2024-05-31 15:54:42 -0400

    Added `get_all_statuses`

--- routes/statuses.py
+++ routes/statuses.py
@@ -2,3 +2,13 @@ from flask import Blueprint, request, jsonify
 from db import get_db_connection
 
 statuses_blueprint = Blueprint('statuses', __name__)
+
+@statuses_blueprint.route('/', methods=['GET'])
+def get_all_statuses():
+    db = get_db_connection()
+    with db.cursor() as cursor:
+        cursor.callproc('get_all_statuses')
+        statuses = cursor.fetchall()
+    db.close()
+
+    return jsonify(statuses)

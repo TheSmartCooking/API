8a7f11c8e2dfa0cb1732b29415e3418373351fba
Vianpyro <vianney@veremme.org>
2024-05-31 14:09:22 -0400

    Updated config

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from `.env` file
+load_dotenv()
+
 class Config:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = ''
-    MYSQL_PASSWORD = ''
-    MYSQL_DB = ''
+    MYSQL_HOST = os.getenv('DB_HOST', 'localhost')
+    MYSQL_USER = os.getenv('DB_USERNAME')
+    MYSQL_PASSWORD = os.getenv('DB_PASSWORD')
+    MYSQL_DB = os.getenv('DB_NAME')
     MYSQL_CURSORCLASS = 'DictCursor'

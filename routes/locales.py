8f8d63e3aa5dcc18223cfef2cd835072d91faecf
Vianpyro <vianney@veremme.org>
2024-05-31 16:00:01 -0400

    Moved `get_comments_by_recipe_id`

--- routes/locales.py
+++ routes/locales.py
@@ -2,3 +2,5 @@ from flask import Blueprint, request, jsonify
 from db import get_db_connection
 
 locales_blueprint = Blueprint('locales', __name__)
+
+@locales_blueprint.route('/<int:person_id>', methods=['GET'])

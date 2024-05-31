655e70391846d9352a99e300250e395cfb75ce71
Vianpyro <vianney@veremme.org>
2024-05-31 16:01:22 -0400

    Added `get_all_locales`

--- routes/locales.py
+++ routes/locales.py
@@ -3,4 +3,12 @@ from db import get_db_connection
 
 locales_blueprint = Blueprint('locales', __name__)
 
-@locales_blueprint.route('/<int:person_id>', methods=['GET'])
+@locales_blueprint.route('/', methods=['GET'])
+def get_all_locales():
+    db = get_db_connection()
+    with db.cursor() as cursor:
+        cursor.callproc('get_all_locales')
+        person = cursor.fetchone()
+    db.close()
+
+    return jsonify(person)

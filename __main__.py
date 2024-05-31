972b072af2b4711dabf733b2c66fb8f0052d821b
Vianpyro <vianney@veremme.org>
2024-05-31 14:12:45 -0400

    Added home message

--- __main__.py
+++ __main__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from config import Config
-from db import get_db_connection
 from routes import register_routes
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+@app.route('/')
+def home():
+    return "Hello there!"
 
 # Register all routes
 register_routes(app)

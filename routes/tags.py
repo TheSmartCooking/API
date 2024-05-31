2f1ca0348f9f0cf256cd2d1a123b78a3f406941c
Vianpyro <vianney@veremme.org>
2024-05-31 15:40:14 -0400

    Added `get_paginated_tags`

--- routes/tags.py
+++ routes/tags.py
@@ -1,4 +1,18 @@
 from flask import Blueprint, request, jsonify
 from db import get_db_connection
+from config import DEFAULT_PAGE, DEFAULT_PAGE_SIZE
 
 tags_blueprint = Blueprint('tags', __name__)
+
+@tags_blueprint.route('/', methods=['GET'])
+def get_paginated_tags():
+    page = int(request.args.get('page', DEFAULT_PAGE))
+    page_size = int(request.args.get('page_size', DEFAULT_PAGE_SIZE))
+
+    db = get_db_connection()
+    with db.cursor() as cursor:
+        cursor.callproc('get_paginated_tags', [page, page_size])
+        tags = cursor.fetchall()
+    db.close()
+
+    return jsonify(tags)

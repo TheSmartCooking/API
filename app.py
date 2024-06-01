bdfe44b5a55c5ed21b6963255adbc56f741327bc
Vianpyro <vianney@veremme.org>
2024-06-01 15:34:37 -0400

    Better logs when running this way

--- app.py
+++ app.py
@@ -19,5 +19,4 @@ register_routes(app)
 register_error_handlers(app)
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=29565)
+    app.run(host='0.0.0.0', port=29565, debug=False)

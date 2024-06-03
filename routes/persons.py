24c897412710a240ebb1ca6ac808fbe4d9d9b70b
Vianney Veremme <10519369+Vianpyro@users.noreply.github.com>
2024-06-03 19:51:15 -0400

    Fixed person endpoints (#9)
    * Fixed endpoints
    
    * Slight change in order

--- routes/persons.py
+++ routes/persons.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required
 
+from config import DEFAULT_PAGE, DEFAULT_PAGE_SIZE
 from db import get_db_connection
 
 persons_blueprint = Blueprint("persons", __name__)
@@ -18,9 +20,12 @@ def get_person_by_id(person_id):
 
 @persons_blueprint.route("/<int:person_id>/recipes", methods=["GET"])
 def get_recipes_by_author(person_id):
+    page = int(request.args.get("page", DEFAULT_PAGE))
+    page_size = int(request.args.get("page_size", DEFAULT_PAGE_SIZE))
+
     db = get_db_connection()
     with db.cursor() as cursor:
-        cursor.callproc("get_recipes_by_author", [person_id])
+        cursor.callproc("get_recipes_by_author", [person_id, page, page_size])
         recipes = cursor.fetchall()
     db.close()
 
@@ -29,9 +34,12 @@ def get_recipes_by_author(person_id):
 
 @persons_blueprint.route("/<int:person_id>/comments", methods=["GET"])
 def get_comments_by_person(person_id):
+    page = int(request.args.get("page", DEFAULT_PAGE))
+    page_size = int(request.args.get("page_size", DEFAULT_PAGE_SIZE))
+
     db = get_db_connection()
     with db.cursor() as cursor:
-        cursor.callproc("get_comments_by_person", [person_id])
+        cursor.callproc("get_comments_by_person", [person_id, page, page_size])
         comments = cursor.fetchall()
     db.close()
 
@@ -39,6 +47,7 @@ def get_comments_by_person(person_id):
 
 
 @persons_blueprint.route("/<int:person_id>/favorites", methods=["GET"])
+@jwt_required
 def get_favorites_by_person_id(person_id):
     db = get_db_connection()
     with db.cursor() as cursor:

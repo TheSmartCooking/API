0a24f80943c886d94cd23fa57e2f1bd546809788
Vianney Veremme <10519369+Vianpyro@users.noreply.github.com>
2024-06-04 18:07:05 -0400

    Added interactions (#13)
    * Added interactions
    
    * Removed unused imports
    
    * Added blueprint
    
    * Lol wtf did I smoke to get a post?

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,5 +1,6 @@
 from .authentication import authentications_blueprint
-from .image import images_blueprint
+from .images import images_blueprint
+from .interactions import interactions_blueprint
 from .locales import locales_blueprint
 from .persons import persons_blueprint
 from .recipes import recipes_blueprint
@@ -10,6 +11,7 @@ from .tags import tags_blueprint
 def register_routes(app):
     app.register_blueprint(authentications_blueprint, url_prefix="/authentication")
     app.register_blueprint(images_blueprint, url_prefix="/image")
+    app.register_blueprint(interactions_blueprint, url_prefix="/interact")
     app.register_blueprint(recipes_blueprint, url_prefix="/recipe")
     app.register_blueprint(persons_blueprint, url_prefix="/person")
     app.register_blueprint(tags_blueprint, url_prefix="/tag")

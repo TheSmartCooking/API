9071b50290fdd6588da47f6d5cacce4caa961051
Vianpyro <vianney@veremme.org>
2024-06-03 22:15:34 -0400

    Fix

--- routes/persons.py
+++ routes/persons.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
 
 from config import DEFAULT_PAGE, DEFAULT_PAGE_SIZE

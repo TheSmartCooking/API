0b342cab0dd47d530d4e81c2fb24e44d8af2bcec
Vianpyro <vianney@veremme.org>
2024-12-07 23:47:40 -0500

    Add recipe routes for retrieving all recipes, recipe by ID, engagements, ingredients, and tags

---
+++ routes/recipe.py
@@ -0,0 +1,56 @@
+from flask import Blueprint, jsonify, request
+
+from config import DEFAULT_PAGE_SIZE
+from db import get_db_connection
+
+recipe_blueprint = Blueprint("recipe", __name__)
+
+
+@recipe_blueprint.route("/all", methods=["GET"])
+def get_all_recipes():
+    db = get_db_connection()
+    with db.cursor() as cursor:
+        cursor.callproc("get_all_recipes")
+        recipes = cursor.fetchall()
+    db.close()
+    return jsonify(recipes)
+
+
+@recipe_blueprint.route("/<int:recipe_id>", methods=["GET"])
+def get_recipe_by_id(recipe_id):
+    db = get_db_connection()
+    with db.cursor() as cursor:
+        cursor.callproc("get_recipe_by_id", (recipe_id,))
+        recipe = cursor.fetchone()
+    db.close()
+    return jsonify(recipe)
+
+
+@recipe_blueprint.route("/<int:recipe_id>/engagements", methods=["GET"])
+def get_recipe_engagement(recipe_id):
+    db = get_db_connection()
+    with db.cursor() as cursor:
+        cursor.callproc("get_recipe_engagement", (recipe_id,))
+        engagement = cursor.fetchone()
+    db.close()
+    return jsonify(engagement)
+
+
+@recipe_blueprint.route("/<int:recipe_id>/ingredients", methods=["GET"])
+def get_recipe_ingredients(recipe_id):
+    db = get_db_connection()
+    with db.cursor() as cursor:
+        cursor.callproc("get_recipe_ingredients", (recipe_id,))
+        ingredients = cursor.fetchall()
+    db.close()
+    return jsonify(ingredients)
+
+
+@recipe_blueprint.route("/<int:recipe_id>/tags", methods=["GET"])
+def get_recipe_tags(recipe_id):
+    db = get_db_connection()
+    with db.cursor() as cursor:
+        cursor.callproc("get_recipe_tags", (recipe_id,))
+        tags = cursor.fetchall()
+    db.close()
+    return jsonify(tags)

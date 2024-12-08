389e4121323db4444c3a508eafb988f32ba71831
Vianpyro <vianney@veremme.org>
2024-12-07 20:29:16 -0500

    Refactor app configuration, enhance JWT handling, and implement response standardization

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 from dotenv import load_dotenv
 from flask_limiter import Limiter
@@ -9,22 +10,31 @@ load_dotenv()
 
 
 class Config:
+    # Database configuration
     MYSQL_HOST = os.getenv("DB_HOST", "host.docker.internal")
     MYSQL_USER = os.getenv("DB_USERNAME", "root")
     MYSQL_PASSWORD = os.getenv("DB_PASSWORD", "myrootpassword")
     MYSQL_DB = os.getenv("DB_NAME", "smartcooking")
     MYSQL_CURSORCLASS = "DictCursor"
 
+    # Pagination configuration
+    DEFAULT_PAGE = 1
+    DEFAULT_PAGE_SIZE = 10
 
-DEFAULT_PAGE = 1
-DEFAULT_PAGE_SIZE = 10
-IMAGES_FOLDER = os.path.join(os.getcwd(), "images")
-VPNAPI_KEY = os.getenv("VPNAPI_KEY")
+    # JWT configuration
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_ACCESS_TOKEN_EXPIRY = timedelta(hours=1)
+    JWT_REFRESH_TOKEN_EXPIRY = timedelta(days=30)
 
-if not os.path.exists(IMAGES_FOLDER):
-    os.makedirs(IMAGES_FOLDER)
+    # Image upload folder
+    IMAGES_FOLDER = os.path.join(os.getcwd(), "images")
+    MAX_CONTENT_LENGTH = 1024 * 1024  # 1 MB
+
+
+if not os.path.exists(Config.IMAGES_FOLDER):
+    os.makedirs(Config.IMAGES_FOLDER)
 
 limiter = Limiter(
     key_func=get_remote_address,
-    default_limits=["1000 per day", "200 per hour", "30 per minute"],
+    default_limits=["1000 per day", "200 per hour", "30 per minute", "3 per second"],
 )

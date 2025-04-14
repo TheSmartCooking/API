4687008f75b7e541530f56fb43d3b6d671b3993b
Vianney Veremme <vianney@veremme.org>
2025-04-14 18:05:09 -0400

    Update Redis host configuration in cache setup

--- config/cache.py
+++ config/cache.py
@@ -3,7 +3,7 @@ from flask_caching import Cache
 cache = Cache(
     config={
         "CACHE_TYPE": "RedisCache",
-        "CACHE_REDIS_HOST": "0.0.0.0",
+        "CACHE_REDIS_HOST": "host.docker.internal",
         "CACHE_REDIS_PORT": 6379,
     }
 )
